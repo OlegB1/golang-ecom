2e7285b83214ef7a2b09ea8487ce3de5f71f5c23
OlegB1 <olegburduzhan@gmil.com>
2025-03-16 23:51:21 +0200

    fix port

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,10 @@ import (
 
 func main() {
 	db := db.NewStorage(config.Envs.DB_ADDR)
+	port := config.Envs.SERVER_ADDR
+	if port == "" {
+		port = "8080"
+	}
 
 	server := api.NewAPIServer(":"+config.Envs.SERVER_ADDR, db)
 	if err := server.Run(); err != nil {
